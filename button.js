198f48e7eef6213fc9afece9bb8c50f79f2d17aa
harshjain9191 <85540450+harshjain9191@users.noreply.github.com>
2021-06-09 10:28:41 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,48 @@
+function see1() {
+    document.getElementById('op1').style.backgroundColor = 'red';
+}
+function see2() {
+    document.getElementById('op2').style.backgroundColor = 'red';
+}
+function see3() {
+    document.getElementById('op3').style.backgroundColor = 'red';
+}
+function see4() {
+    document.getElementById('op4').style.backgroundColor = 'red';
+}
+function see5() {
+    document.getElementById('op5').style.backgroundColor = 'red';
+}
+function see6() {
+    document.getElementById('op6').style.backgroundColor = 'red';
+}
+function see7() {
+    document.getElementById('op7').style.backgroundColor = 'red';
+}
+function see8() {
+    document.getElementById('op8').style.backgroundColor = 'red';
+}
+function see9() {
+    document.getElementById('op9').style.backgroundColor = 'red';
+}
+function see10() {
+    document.getElementById('op10').style.backgroundColor = 'red';
+}
+function see11() {
+    document.getElementById('op11').style.backgroundColor = 'red';
+}
+function see12() {
+    document.getElementById('op12').style.backgroundColor = 'red';
+}
+function see13() {
+    document.getElementById('op13').style.backgroundColor = 'red';
+}
+function see14() {
+    document.getElementById('op14').style.backgroundColor = 'red';
+}
+function see15() {
+    document.getElementById('op15').style.backgroundColor = 'red';
+}
+function see16() {
+    document.getElementById('op16').style.backgroundColor = 'red';
+}
